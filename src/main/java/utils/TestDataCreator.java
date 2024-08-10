d5e03ea1445a85a6e5ba17c5be92608b575877bc
nicolasortiz1995 <vinico0911@hotmail.com>
2024-08-10 17:24:22 -0500

    Creación archivo 'TestRunner' y primer escenario '.feature'

--- src/main/java/utils/TestDataCreator.java
+++ src/main/java/utils/TestDataCreator.java
@@ -2,49 +2,105 @@ package utils;
 
 import com.github.javafaker.Faker;
 
+/**
+ * The {@code TestDataCreator} class is a utility class that provides methods for generating
+ * random test data using the Faker library. This class includes methods for creating 
+ * random IDs, usernames, first names, last names, email addresses, passwords, and phone numbers.
+ * The generated data can be used in testing scenarios where unique or random values are needed.
+ */
 public class TestDataCreator {
 
+    /**
+     * An instance of the Faker class used to generate random data.
+     */
     private static Faker faker = new Faker();
 
+    /**
+     * Generates a random integer ID between 0 and 10,000.
+     *
+     * @return a random integer ID.
+     */
     public static int getID() {
-        /**
-         * Returns an ID value as an integer
-         */
-        return faker.number().numberBetween(0,10000);
+        return faker.number().numberBetween(0, 10000);
     }
 
-    public static String getUserName(){
+    /**
+     * Generates a random username.
+     *
+     * @return a random username as a {@code String}.
+     */
+    public static String getUserName() {
         return faker.name().username();
     }
 
-    public static String getFirstName(){
+    /**
+     * Generates a random first name.
+     *
+     * @return a random first name as a {@code String}.
+     */
+    public static String getFirstName() {
         return faker.name().firstName();
     }
 
-    public static String getLastName(){
+    /**
+     * Generates a random last name.
+     *
+     * @return a random last name as a {@code String}.
+     */
+    public static String getLastName() {
         return faker.name().lastName();
     }
 
-    public static String getEmailAdress(){
+    /**
+     * Generates a random email address.
+     *
+     * @return a random email address as a {@code String}.
+     */
+    public static String getEmailAdress() {
         return faker.internet().emailAddress();
     }
 
-    public static String getPassword(){
+    /**
+     * Generates a random password.
+     *
+     * @return a random password as a {@code String}.
+     */
+    public static String getPassword() {
         return faker.internet().password();
     }
 
-    public static String getPhone(){
+    /**
+     * Generates a random phone number.
+     *
+     * @return a random phone number as a {@code String}.
+     */
+    public static String getPhone() {
         return faker.phoneNumber().phoneNumber();
     }
 
+    /**
+     * Returns the current {@code Faker} instance.
+     *
+     * @return the current {@code Faker} instance.
+     */
     public static Faker getFaker() {
         return faker;
     }
 
+    /**
+     * Sets a new {@code Faker} instance.
+     *
+     * @param faker the new {@code Faker} instance to set.
+     */
     public static void setFaker(Faker faker) {
         TestDataCreator.faker = faker;
     }
 
+    /**
+     * Returns a string representation of the {@code TestDataCreator} class.
+     *
+     * @return a string representation of the {@code TestDataCreator} class.
+     */
     @Override
     public String toString() {
         return "TestDataCreator []";
---
+++ src/test/java/features/CreateUser.feature
@@ -0,0 +1,29 @@
+Feature: User Management
+
+  Background: The Request Body Configuration
+    * url baseUrl
+    * def requestPayload = read('classpath:payload/CreateUserPayload.json')
+    * set requestPayload.id = Java.type('utils.TestDataCreator').getID()
+    * set requestPayload.username = Java.type('utils.TestDataCreator').getUserName()
+    * set requestPayload.firstName = Java.type('utils.TestDataCreator').getFirstName()
+    * set requestPayload.lastName = Java.type('utils.TestDataCreator').getLastName()
+    * set requestPayload.email = Java.type('utils.TestDataCreator').getEmailAdress()
+    * set requestPayload.password = Java.type('utils.TestDataCreator').getPassword()
+    * set requestPayload.phone = Java.type('utils.TestDataCreator').getPhone()
+    * set requestPayload.userStatus = Java.type('utils.TestDataCreator').getStatusInt()[0]
+
+  Scenario: Create an user
+    Given header Content-Type = 'application/json'
+    And path '/v2/user'
+    And request requestPayload
+    When method post
+    Then status 200
+    And match $.code == 200
+    And match $.type == 'unknown'
+    And match $.message != null
+    * print response
+
+
+
+
+
