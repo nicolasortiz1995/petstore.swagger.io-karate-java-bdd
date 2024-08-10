ceb9a234247e9012a68adb3ec2be082309f46a93
nicolasortiz1995 <vinico0911@hotmail.com>
2024-08-09 22:31:45 -0500

    Creación archivo 'karate-config.js'

--- src/main/java/utils/TestDataCreator.java
+++ src/main/java/utils/TestDataCreator.java
@@ -37,68 +37,6 @@ public class TestDataCreator {
         return faker.phoneNumber().phoneNumber();
     }
 
-    
-    
-    public static String getDogName() {
-        /**
-         * Returns a dog name
-         */
-        return faker.dog().name();
-    }
-
-    public static String getDogCategoryName() {
-        /**
-         * Returns a dog category name
-         */
-        return faker.dog().breed();
-    }
-
-    public static int getDogAge() {
-        /**
-         * Returns a dog age
-         */
-        return Integer.parseInt(faker.dog().age());
-
-    }
-
-    public static int[] getStatusInt() {
-        /**
-         * Returns a status
-         */
-        return new int[]{0, 1, 2};
-    }
-    
-
-    public static String getCatName() {
-        /**
-         * Returns a cat name
-         */
-        return faker.cat().name();
-    }
-
-    public static String getCatCategoryName() {
-        /**
-         * Returns a cat category name
-         */
-        return faker.cat().breed();
-    }
-
-    public static String getFileName() {
-        /**
-         * Returns a file name
-         */
-        return faker.file().fileName();
-    }
-
-
-
-    public static String[] getStatus() {
-        /**
-         * Returns a status
-         */
-        return new String[]{"available", "pending", "sold"};
-    }
-
     public static Faker getFaker() {
         return faker;
     }
